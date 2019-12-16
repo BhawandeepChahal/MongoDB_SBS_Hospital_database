7a6a01880df6d1606c8c5eb79fd5aee4559d5c74
BhawandeepChahal <46692844+BhawandeepChahal@users.noreply.github.com>
2019-12-16 14:28:31 -0500

    Creation of Room_Type Table

--- Project_Enterprise_Bhawandeep.js
+++ Project_Enterprise_Bhawandeep.js
@@ -2,19 +2,40 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://Simar:Sunny123gg@cluster0-xbxcz.mongodb.net/test";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:true });
 client.connect(err => {
-  const collection = client.db("SBS_Hospital").collection("Laboratory_Staff");
+  /*
   
-  var insertLab=[{Staff_ID:501,FullName:"Rajni Kant",Gender:"M",Position:"Lab Technician",SalaryPH:20,SSN:917899024,Email:"rjk910@sbshosp.com",Phone:9102907831},
-				{Staff_ID:502,FullName:"Kartik Aryan",Gender:"M",Position:"Clinical Manager",SalaryPH:19.75,SSN:9182938054,Email:"karyan159@sbshosp.com",Phone:1009876492},
-				{Staff_ID:503,FullName:"Sushant Rajput",Gender:"M",Position:"Lab Assistant",SalaryPH:15.50,SSN:998701928,Email:"ssr888@sbshosp.com",Phone:1093948010},
-				{Staff_ID:504,FullName:"Alia Bhatt",Gender:"F",Position:"Blood Donor Recruiter",SalaryPH:23,SSN:910992009,Email:"bhatt0101@sbshosp.com",Phone:1953180101},
-				{Staff_ID:505,FullName:"Johny Lever",Gender:"M",Position:"Blood Bank Manager",SalaryPH:30.25,SSN:999423654,Email:"ljo5278@sbshosp.com",Phone:1192526751}];
+  const collection = client.db("SBS_Hospital").collection("Nurses");
+  
+  var insertNurses=[{Nurse_ID:201,FullName:"Preity Zinta",Gender:"F",Position:"Head Nurse",SalaryPH:35,SSN:91845654,Email:"pz123@sbshosp.com",Phone:1029307831},
+						{Nurse_ID:202,FullName:"Krysten Stewart",Gender:"F",Position:"Nurse",SalaryPH:28,SSN:929471054,Email:"kry109@sbshosp.com",Phone:1029327492},
+						{Nurse_ID:203,FullName:"Catriona Gray",Gender:"F",Position:"Nurse",SalaryPH:25,SSN:998761234,Email:"muo234@sbshosp.com",Phone:1029381010},
+						{Nurse_ID:204,FullName:"Rajpal Yadav",Gender:"M",Position:"Nurse",SalaryPH:25,SSN:910293709,Email:"rajya817@sbshosp.com",Phone:102101011},
+						{Nurse_ID:205,FullName:"Sushmita  Sen",Gender:"F",Position:"Nurse",SalaryPH:31,SSN:900023654,Email:"sushi65@sbshosp.com",Phone:1192123451}];
 						
-	collection.insertMany(insertLab,function(err,result){
+	collection.insertMany(insertNurses,function(err,result){
 		if(err) throw err;
 		
-	console.log("Inserted 5 rows into Laboratory_Staff table..");
+	console.log("Inserted 5 rows into Nurses table..");
+  */
+  
+  const collection = client.db("SBS_Hospital").collection("Room_Type");
+  
+  var insertRoom=[{Room_Number:"1A",Type:"ICU",No_Of_Beds:12,Floor:1,RoomCostPerHour:120},
+				{Room_Number:"2A",Type:"Emergency Ward",No_Of_Beds:25,Floor:1,RoomCostPerHour:70},
+				{Room_Number:"3B",Type:"Operation Theatre",No_Of_Beds:1,Floor:2,RoomCostPerHour:80},
+				{Room_Number:"4B",Type:"Casualty",No_Of_Beds:15,Floor:2,RoomCostPerHour:50},
+				{Room_Number:"5C",Type:"Consulting Room",No_Of_Beds:1,Floor:3,RoomCostPerHour:10}];
+						
+	collection.insertMany(insertRoom,function(err,result){
+		if(err) throw err;
+		
+	console.log("Inserted 5 rows into Room table..");
   
   client.close();
 });
+
 });
+
+
+
+
